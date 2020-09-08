628fcb7149ad452ed0c948402618a4b8d2b30d66
Ivailo <paradoxa624@gmail.com>
2020-09-08 15:17:46 +0300

    finished BlackJack

--- Week 5 - HTML, CSS/MyBlackJack/main.js
+++ Week 5 - HTML, CSS/MyBlackJack/main.js
@@ -2,6 +2,8 @@ let suites = ["spades","hearts","clubs","diamonds"];
 let values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
 
 const deck = makeDeck();
+score = 0;
+let screen_score = document.getElementById('score');
 
 let player_hand = document.getElementById('p_cards');
 let dealer_hand = document.getElementById('d_cards');
@@ -17,7 +19,6 @@ let quit_btn = document.getElementById('quit');
 let hit_btn = document.getElementById('hit');
 let stand_btn = document.getElementById('stand');
 
-
 function makeDeck() {
     let deck = new Array();
 
@@ -29,6 +30,14 @@ function makeDeck() {
     }
     
     return deck;
+}
+
+function dealer_draw() {
+    dealer_hand.removeChild(dealer_hand.children[1]);
+    while(parseInt(dealer_hand_value.innerText) < 17) {
+        hand_value_increase(dealer_hand_value);
+        draw(deck,dealer_hand);
+    }
 }
 
 
@@ -64,7 +73,7 @@ function hand_value_increase(hand_value) {
         return;
     }
 
-    if(deck[0].Value == "A" && value + 11 < 21) {
+    if(deck[0].Value == "A" && value + 11 <= 21) {
         value += 11;
         hand_value.innerText = value;
         return;
@@ -81,6 +90,66 @@ function hand_value_increase(hand_value) {
     return;
 }
 
+function win_game() {
+
+    
+    screen_score.innerText = "You won!"
+
+    setTimeout(function() {
+        score++;
+        screen_score.innerText = "Score:  " + score;}, 2000);
+
+    setTimeout(clear_board, 3000);
+    setTimeout(start_new_game, 3000);
+}
+
+function lose_game() {
+
+    screen_score.innerText = "You lost!";
+ 
+     setTimeout(function() {
+         score--;
+         screen_score.innerText = "Score:  " + score;}, 2000);
+
+    setTimeout(clear_board, 5000);
+    setTimeout(start_new_game, 5000);
+}
+
+function clear_board() {
+
+    while(player_hand.firstChild){
+        player_hand.removeChild(player_hand.firstChild);
+    }
+
+    while(dealer_hand.firstChild){
+        dealer_hand.removeChild(dealer_hand.firstChild);
+    }
+
+    player_hand_value.innerText = 0;
+    dealer_hand_value.innerText = 0;
+}
+
+function start_new_game() {
+    hand_value_increase(player_hand_value);
+    draw(deck,player_hand);
+    hand_value_increase(player_hand_value);
+    draw(deck,player_hand);
+
+    hand_value_increase(dealer_hand_value);
+    draw(deck,dealer_hand);
+
+    let back = new Image;
+    back.src = 'cards/back.jpg';
+    dealer_hand.appendChild(back);
+
+
+    if(player_hand_value.innerText == 21){
+        screen_score.innerText = "BlackJack!"
+        win_game();
+        return;
+    }
+}
+
 start_btn.addEventListener('click', () => {
     
     start_btn.disabled = true;
@@ -94,34 +163,42 @@ start_btn.addEventListener('click', () => {
     
     player_hand_value = document.getElementById('p_value');
 
-    hand_value_increase(player_hand_value);
-    draw(deck,player_hand);
-    hand_value_increase(player_hand_value);
-    draw(deck,player_hand);
-    hand_value_increase(dealer_hand_value);
-    draw(deck,dealer_hand);
-
+    start_new_game();
 });
 
 quit_btn.addEventListener('click', () => {
-    player_console.style.visibility='hidden'
+    player_console.style.visibility='hidden';
     quit_btn.disabled = true;
     start_btn.disabled = false;
-    
-    player_hand_value.innerText = 0;
-    dealer_hand_value.innerText = 0;
-    
-    while(player_hand.firstChild){
-        player_hand.removeChild(player_hand.firstChild);
-    }
-
-    while(dealer_hand.firstChild){
-        dealer_hand.removeChild(dealer_hand.firstChild);
-    }
-    
-
+    score = 0;
+    screen_score.innerText = "Score: 0";
+    clear_board();
 });
 
 hit_btn.addEventListener('click', () => {
-    
+    hand_value_increase(player_hand_value);
+    draw(deck,player_hand);
+
+    if(parseInt(player_hand_value.innerText) > 21) {
+        dealer_draw();
+        lose_game();
+        return;
+    }
+});
+
+stand_btn.addEventListener('click', () => {
+    dealer_draw();
+    if (parseInt(dealer_hand_value.innerText) > 21) {
+        win_game();
+        return;
+    }
+
+    if(parseInt(dealer_hand_value.innerText) > parseInt(player_hand_value.innerText)) {
+        lose_game();
+        return;
+    }
+    else {
+        win_game();
+        return;
+    }
 });
